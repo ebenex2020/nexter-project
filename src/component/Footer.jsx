aca13d3ed3e1a0276ae7c619adb31ff03166cdf2
Ebenezer Ajewole <ebenex20202@gmail.com>
2023-12-04 13:56:41 +0200

    footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,29 @@
 /** @format */
 
+import { NavLink } from "react-router-dom";
 import styles from "../Css/Footer.module.css";
 
+const links = [
+  { text: "find your dream home" },
+  { text: "request proposal" },
+  { text: "download home planner" },
+  { text: "contact us" },
+  { text: "submit your property" },
+  { text: "come work with us" },
+];
+
 function Footer() {
-  return <div className={styles.footer}>footer</div>;
+  return (
+    <div className={styles.footer}>
+      <ul>
+        {links.map((link, i) => (
+          <li key={i}>
+            <NavLink>{link.text}</NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Footer;
